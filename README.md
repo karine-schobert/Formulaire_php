b7e6b3d53299e9a8a4b0e55f975b3332cdbf820b
karine-schobert <schobert.karine@sfr.fr>
2022-12-29 11:02:54 +0100

    correction html, creation du README, ajout image, mise en place index.php

---
+++ README.md
@@ -0,0 +1,51 @@
+#FORMULAIRE EN PHP
+---
+##Tecnhiques utilisées pour la creation du formulaire
+
+    -css et html pour être sur de la mise en forme du formulaire
+    -création d'une page PHP pour recuperer les données du formulaire
+    
+## OBJECTIFS 
+
+- Revoir la syntaxe PHP
+- Ecrire le script correspondant
+- Utiliser le formulaire 
+- Savoir utiliser les conditions pour executer le traitement des données du 
+  formulaire
+---
+## PROBLEMATIQUE
+
+- Inscription des eleves dans un groupe scolaire de la primaire au lycée
+- Faire apparaitre le nom et prénom de l'enfant.
+- A chaque âge correspond un niveau d'étude (primaire, college, lycee).
+- Prendre en compte l'option langue que pour le college et lycée.
+- Prendre en compte l'inscription a la cantine.
+
+---
+##INSTRUCTIONS
+
+###les ages a prendre en compte 
+- Age pour la primaire de 6 à 9 ans
+- Age pour le college de 10 à 15 ans
+- Age pour le lycée de 16 à 20 ans
+  
+###Niveau d'études à prendre en compte pour la langue
+    - Faire une choix obligatoire pour le college en LV1
+       *LV1
+
+    -Faire une choix obligatoire pour le lycee en LV1 Et LV2
+       *LV1
+       *LV2
+
+###Le choix de la cantine 
+    - Cocher obligatoirement la case : soit OUI soit Non
+  ---
+
+### RESULTATS
+
+    L'affichage des reponses doit s'afficher sur la page:
+        Votre inscription .....
+---
+## Remarques
+
+- debutante en html , css et php
--- php/index.php
+++ php/index.php
@@ -1,75 +1,89 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Permis de conduire</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../css/style.css">
-    </head>
+
+<head>
+    <meta charset="utf-8">
+    <title>Permis de conduire</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
 
 
-    <body>
+<body>
+    <main>
+        <div class="Container_Formulaire">
+            <section class="formulaire-inscription">
+                <h1>Inscription dans notre établissement</h1>
+                <h2>Groupe scolaire KS primaire-college-lyçée</h2>
 
-        <main>
-                <div class="Container_Formulaire">
-                        <section class="formulaire-inscription">
-                            <h1>Inscription dans notre établissement</h1>
-                            <h2>Groupe scolaire KS primaire-college-lyce</h2>
+                <form action="" method="POST">
 
-                            <form action ="" method="GET">
+                    <label for="lastname">Nom</label>
+                    <input type="text" name="lastname" id="lastname">
 
-                            <label for="lastname">Nom</label>
-                            <input type="text" name="lastname" id="lastname">
+                    <label for="firstname">Prénom</label>
+                    <input type="text" name="firstname" id="firstname">
 
-                            <label for="firstname">Prénom</label>
-                            <input type="text" name="firstname" id="firstname">
+                    <label for="age">Âge</label>
+                    <input type="number" name="age" id="age">
 
-                            <label for="age">Âge</label>
-                            <input type="number" name="age" id="age">
+                    <fieldset>
+                        <legend>Options langues: <input type="checkbox" id="LV1" name="Option" value="LV1">
+                            <label for="LV1">LV1</label> <input type="checkbox" id="LV2" name="Option"
+                                value="LV2"><label for="LV2">LV2</label>
+                        </legend>
+                        <div>
+                            <input type="checkbox" id="Anglais" name="Option" value="Anglais">
+                            <label for="Anglais">Anglais</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="Allemand" name="Option" value="Allemand">
+                            <label for="Allemand">Allemand</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="Espagnole" name="Option" value="Espagnole">
+                            <label for="Espagnole">Espagnole</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="Italien" name="Option" value="Italien">
+                            <label for="Italien">Italien</label>
+                        </div>
+                    </fieldset>
 
-                            <fieldset>
-                                        <legend>Quels sont vos centres d'intérêts :</legend>
-                                        <div>
-                                            <input type="checkbox" id="music" name="interest" value="music">
-                                            <label for="music">Musique</label>
-                                        </div>
-                                        <div>
-                                            <input type="checkbox" id="cinema" name="interest" value="cinema">
-                                            <label for="cinema">Cinéma</label>
-                                        </div>
-                                            <div>
-                                            <input type="checkbox" id="sport" name="interest" value="sport">
-                                            <label for="music">Sport</label>
-                                        </div>
-                                            <div>
-                                            <input type="checkbox" id="fashion" name="interest" value="fashion">
-                                            <label for="music">Mode</label>
-                                        </div>
-                                            <div>
-                                            <button type="submit">Envoyer</button>
-                                        </div>
-                                        </fieldset>
-                            <button>Je m'inscris</button>
-                            </form>
+                    <fieldset>
+                        <legend>Cantine
+                        </legend>
+                        <div>
+                            <input type="checkbox" id="oui" name="Choix" value="oui">
+                            <label for="oui">oui</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="non" name="choix" value="non">
+                            <label for="non">Non</label>
+                        </div>
+                    </fieldset>
 
-                        </section>
+                    <button>Je m'inscris</button>
+                </form>
 
-                        <section class= "formulaire-inscription-resume" >
-                                 <h1>Résumé de l'inscription</h1>
-                                 <h2>Inscription de </h2>
-                                    <section class="answer">
-                                    </section>
-                        </section>
+            </section>
 
-                </div>
+            <section class="formulaire-resume">
+                <h1>Votre inscription au </h1>
+                <h2>Inscription de </h2>
+                <h3>Etablissement</h3>
+                <h4>Langue Vivante ..... Option ..... </h4>
+                <h5>Inscription cantine</h5>
 
-        </main>
+                <section class="answer">
 
+                </section>
+            </section>
 
+        </div>
 
+    </main>
+</body>
 
-
-
-    </body>
 </html>
