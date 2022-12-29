6a1fcd522bb79ac678d7decefa503bf164449e90
karine schobert <schobert.karine@sfr.fr>
2022-12-29 09:54:54 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Permis de conduire</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+
+
+    <body>
+
+        <main>
+                <div class="Container_Formulaire">
+                        <section class="formulaire-inscription">
+                            <h1>Inscription dans notre établissement</h1>
+                            <h2>Groupe scolaire KS primaire-college-lyce</h2>
+
+                            <form action ="" method="GET">
+
+                            <label for="lastname">Nom</label>
+                            <input type="text" name="lastname" id="lastname">
+
+                            <label for="firstname">Prénom</label>
+                            <input type="text" name="firstname" id="firstname">
+
+                            <label for="age">Âge</label>
+                            <input type="number" name="age" id="age">
+
+                            <fieldset>
+                                        <legend>Quels sont vos centres d'intérêts :</legend>
+                                        <div>
+                                            <input type="checkbox" id="music" name="interest" value="music">
+                                            <label for="music">Musique</label>
+                                        </div>
+                                        <div>
+                                            <input type="checkbox" id="cinema" name="interest" value="cinema">
+                                            <label for="cinema">Cinéma</label>
+                                        </div>
+                                            <div>
+                                            <input type="checkbox" id="sport" name="interest" value="sport">
+                                            <label for="music">Sport</label>
+                                        </div>
+                                            <div>
+                                            <input type="checkbox" id="fashion" name="interest" value="fashion">
+                                            <label for="music">Mode</label>
+                                        </div>
+                                            <div>
+                                            <button type="submit">Envoyer</button>
+                                        </div>
+                                        </fieldset>
+                            <button>Je m'inscris</button>
+                            </form>
+
+                        </section>
+
+                        <section class= "formulaire-inscription-resume" >
+                                 <h1>Résumé de l'inscription</h1>
+                                 <h2>Inscription de </h2>
+                                    <section class="answer">
+                                    </section>
+                        </section>
+
+                </div>
+
+        </main>
+
+
+
+
+
+
+    </body>
+</html>
