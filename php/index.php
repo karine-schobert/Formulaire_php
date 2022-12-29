5f3823fb588e3b892f2aa2a6637dfdf9f2c78548
karine-schobert <schobert.karine@sfr.fr>
2022-12-29 12:06:34 +0100

    mise en place de la verification du formulaire rempli, de la soumission du formulaire soit validée, condition de l'age pour le type d'etablissement

--- php/index.php
+++ php/index.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
+    <title>Formulaire d'inscription scolaire</title>
 </head>
 
 
@@ -70,14 +71,48 @@
             </section>
 
             <section class="formulaire-resume">
+
+            <!-- Verifier que le formulaire soit bien rempli et qu'il ne soit pas vide-->
+            <?php
+            if(isset($_POST) &&!empty($_POST)){
+            //     var_dump($_POST);
+            }
+             ?>
                 <h1>Votre inscription au </h1>
                 <h2>Inscription de </h2>
+                  <?php
+                //   Verification que le formulaire est bien été soumis  nom prenom age apparition page de droite 
+                if(isset($_POST)&& ($_POST)){
+                    $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
+                    $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
+                    $age = filter_input(INPUT_POST,'age', FILTER_SANITIZE_NUMBER_INT);
+                    echo '<span>'. $lastname .' '. $firstname . ' '.$age . '<pan>';
+                   
+                }
+                ?>
                 <h3>Etablissement</h3>
+                <?php
+                // verification de l'age pour l'etablissement (primaire, college, lycee)
+                if(isset($_POST)&& ($_POST)){
+                    $age =  $age = filter_input(INPUT_POST,'age', FILTER_SANITIZE_NUMBER_INT);
+                    if ($age >=6 && $age <=9){
+                        echo "<span> Bienvenu en primaire  la primaire </span>";
+                    }elseif($age >=10 && $age <= 15 ){
+                        echo "<span> Bienvenu au college </span>";
+                    }elseif ($age >=16 && $age <= 20){
+                        echo "<span> Bienvenu au lycée </span>";
+                    }else {
+                        echo "<span> Desole tu ne peux pas t'inscrire </span>";
+                    }
+
+                }
+                ?>
                 <h4>Langue Vivante ..... Option ..... </h4>
                 <h5>Inscription cantine</h5>
 
                 <section class="answer">
-
+                
+              
                 </section>
             </section>
 
